6af8e4e8c0102f303d8f5657a968f111974695e8
Adrien Morin <morin.adrien@gmail.com>
2022-05-18 12:28:11 +0200

    ADD full crud

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Movie;
 use App\Form\MovieType;
+use App\Repository\MovieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,17 +14,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class MovieController extends AbstractController
 {
     #[Route('/movie', name: 'movie_index')]
-    public function index(EntityManagerInterface $manager): Response
+    public function index(MovieRepository $movieRepository): Response
     {
-        $movies = $manager->getRepository(Movie::class)->findAll();
-
         return $this->render('movie/index.html.twig', [
-            'movies' => $movies
+            'movies' => $movieRepository->findAll()
         ]);
     }
 
     #[Route('/movie/create', name:'movie_create')]
-    public function create(Request $request, EntityManagerInterface $manager) {
+    public function create(Request $request, EntityManagerInterface $manager): Response
+    {
         $movie = new Movie();
         $form = $this->createForm(MovieType::class, $movie);
 
@@ -42,17 +42,43 @@ class MovieController extends AbstractController
         ]);
     }
 
-    #[Route('/movie/{id}', name: 'movie_show', requirements: ['id'=> '\d+'])]
-    public function show($id, EntityManagerInterface $manager): Response
+    #[Route('/movie/update/{name}', name:'movie_update')]
+    public function update(Movie $movie, Request $request, EntityManagerInterface $manager): Response
     {
-        $movie = $manager->getRepository(Movie::class)->find($id);
+        $form = $this->createForm(MovieType::class, $movie);
 
-        if (!$movie) {
-            throw $this->createNotFoundException('The movie does not exist');
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->flush();
+
+            return $this->redirectToRoute('movie_show', [
+                'id' => $movie->getId()
+            ]);
         }
 
+        return $this->render('movie/update.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/movie/{name}', name: 'movie_show')]
+    public function show(Movie $movie): Response
+    {
         return $this->render('movie/show.html.twig', [
             'movie' => $movie
         ]);
+    }
+
+    #[Route('/movie/delete/{name}/{token}', name: 'movie_delete')]
+    public function delete(Movie $movie, $token, EntityManagerInterface $manager): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $movie->getId(), $token)) {
+            $manager->remove($movie);
+            $manager->flush();
+
+            return $this->redirectToRoute('movie_index');
+        }
+
+        return $this->createAccessDeniedException('Wesh!!!');
     }
 }
